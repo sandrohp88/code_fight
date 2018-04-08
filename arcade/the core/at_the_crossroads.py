42d5f2fbe4d1cfc839bf8f9f64bbc382d6690ce3
Sandro Herrera Pallares <sandrohp88@gmail.com>
2018-04-07 17:32:25 -0700

    At the cross roads finished

--- arcade/the core/at_the_crossroads.py
+++ arcade/the core/at_the_crossroads.py
@@ -173,13 +173,81 @@ def tennisSet(score1, score2):
         return True
     else:
         return False
-   
+
+# Once Mary heard a famous song, and a line from it stuck in her head. 
+# That line was "Will you still love me when I'm no longer young and beautiful?". 
+# Mary believes that a person is loved if and only if he/she is both young and beautiful, 
+# but this is quite a depressing thought, so she wants to put her belief to the test.
+
+# Knowing whether a person is young, beautiful and loved, find out if they contradict Mary's belief.
+
+# A person contradicts Mary's belief if one of the following statements is true:
+
+# they are young and beautiful but not loved;
+# they are loved but not young or not beautiful.
+# Example
+
+# For young = true, beautiful = true and loved = true, the output should be
+# willYou(young, beautiful, loved) = false.
+
+# Young and beautiful people are loved according to Mary's belief.
+
+# For young = true, beautiful = false and loved = true, the output should be
+# willYou(young, beautiful, loved) = true.
+
+# Mary doesn't believe that not beautiful people can be loved.
+
+def willYou(young, beautiful, loved):
+    if (young and beautiful) and (not loved):
+        return True
+    if loved and ((not young )or (not beautiful)):
+        return True
+    return False
+
+# You just bought a public transit card that allows 
+# you to ride the Metro for a certain number of days.
+# Here is how it works: upon first receiving the card, 
+# the system allocates you a 31-day pass, which equals the 
+# number of days in January. The second time you pay for the card,
+# your pass is extended by 28 days, i.e. the number of days in February
+# (note that leap years are not considered), and so on. The 13th time
+# you extend the pass, you get 31 days again.
+
+# You just ran out of days on the card, and unfortunately
+# you've forgotten how many times your pass has been extended
+# so far. However, you do remember the number of days you were
+# able to ride the Metro during this most recent month.
+# Figure out the number of days by which your pass will
+# now be extended, and return all the options as an array sorted in increasing order.
+
+# Example
+
+# For lastNumberOfDays = 30, the output should be
+# metroCard(lastNumberOfDays) = [31].
+
+# There are 30 days in April, June, September and November,
+# so the next months to consider are May, July, October or December.
+# All of them have exactly 31 days, which means that you will
+# definitely get a 31-days pass the next time you extend your card.
+def metroCard(lastNumberOfDays):
+    # 28 February
+    # 30 April, June, September, November
+    # 31 January, March, May, July, August, October, December
+    days = [28,30,31]
+    # Returns March, always 31 days
+    if lastNumberOfDays == 30 or lastNumberOfDays == 28:
+        return days[-1:]
+    else:
+        return days
+    
 
 def main():
     # print(reachNextLevel(10,15,4))
     #  print(knapsackLight(10,5,6,4,9))
     # print(isInfiniteProcess(2,3))
-    print(tennisSet(5,8))
+    # print(tennisSet(5,8))
+    # print(willYou(True,False,True))
+    print(metroCard(31))
 
 if __name__ == '__main__':
     main()
