cd8eb8c72d6e4b03fcfe1f08f268475701c84326
Sandro Herrera Pallares <sandrohp88@gmail.com>
2018-05-10 15:37:47 -0700

    slithering in strings finished!

--- arcade/python/slithering_in_strings.py
+++ arcade/python/slithering_in_strings.py
@@ -1,3 +1,7 @@
+import re
+import textwrap as tw
+import string
+
 # One of your friends has an awful writing style: he almost never starts a message with a capital letter, but adds uppercase letters in random places throughout the message. It makes chatting with him very difficult for you, so you decided to write a plugin that will change each message received from your friend into a more readable form.
 
 # Implement a function that will change the very first symbol of the given message to uppercase, and make all the other letters lowercase.
@@ -36,8 +40,156 @@ def catWalk(code):
 def convertTabs(code, x):
     return code.replace("\t",x*' ')
 
+# Unfortunately it looks like the feedbacks page is far from perfect: 
+# each feedback is displayed as a one-line string, and if it's too 
+# long there's no way to see what it is about. Naturally, this 
+# horrible bug should be fixed. Implement a function that, given 
+# a feedback and the size of the screen, splits the feedback into lines so that:
+
+# each token (i.e. sequence of non-whitespace characters) belongs to one of the lines entirely;
+# each line is at most size characters long;
+# no line has trailing or leading spaces;
+# each line should have the maximum possible length, assuming that 
+# all lines before it were also the longest possible.
+# Example
+
+# For feedback = "This is an example feedback", and size = 8,
+# the output should be
+
+# feedbackReview(feedback, size) = ["This is", 
+#                                   "an", 
+#                                   "example", 
+#                                   "feedback"]
+
+
+def feedbackReview(feedback, size):
+    return tw.wrap(feedback,size)
+
+# Given a word, check whether it is a palindrome or not. A string is considered to be a palindrome if it reads the same in both directions.
+
+# Example
+
+# For word = "aibohphobia", the output should be
+# isWordPalindrome(word) = true;
+
+# For word = "hehehehehe", the output should be
+# isWordPalindrome(word) = false.
+
+def isWordPalindrome(word):
+    return word == word[::-1]
+
+
+# You found your very first laptop in the attic, and decided 
+# to give in to nostalgia and turn it on. The laptop turned 
+# out to be password protected, but you know how to crack it: 
+# you have always used the same password, but encrypt it using 
+# permutation ciphers with various keys. The key to the cipher 
+# used to protect your old laptop very conveniently happened to be written on the laptop lid.
+
+# Here's how permutation cipher works: the key to it consists of 
+# all the letters of the alphabet written up in some order. 
+# All occurrences of letter 'a' in the encrypted text are 
+# substituted with the first letter of the key, all occurrences 
+# of letter 'b' are replaced with the second letter from the key, 
+# and so on, up to letter 'z' replaced with the last symbol of the key.
+
+# Given the password you always use, your task is to encrypt 
+# it using the permutation cipher with the given key.
+
+# Example
+
+# For password = "iamthebest" and
+# key = "zabcdefghijklmnopqrstuvwxy", the output should be
+# permutationCipher(password, key) = "hzlsgdadrs".
+
+# Here's a table that can be used to encrypt the text:
+
+# abcdefghijklmnopqrstuvwxyz
+# ||  |  ||   |     || 
+# vv  v  vv   v     vv
+# zabcdefghijklmnopqrstuvwxy
+
+def permutationCipher(password, key):
+    table = str.maketrans(string.ascii_lowercase,key)
+    return password.translate(table)
+
+# The World Wide Competitive Eating tournament is going to be held in your town, 
+# and you're the one who is responsible for keeping track of time. For the great 
+# finale, a large billboard of the given width will be installed on the main square,
+# where the time of possibly new world record will be shown.
+
+# The track of time will be kept by a float number. 
+# It will be displayed on the board with the set precision 
+# precision with center alignment, and it is guaranteed 
+# that it will fit in the screen. Your task is to test the billboard. 
+# Given the time t, the width of the screen and the precision 
+# with which the time should be displayed, return a string that 
+# should be shown on the billboard.
+
+# Example
+
+# For t = 3.1415, width = 10 and precision = 2,
+# the output should be
+# competitiveEating(t, width, precision) = "   3.14   ".
+
+def competitiveEating(t, width, precision):
+    return '{time:.{precision}f}'.format(time = t,precision = precision).center(width, ' ')
+
+# You came to work in a big company as a Senior Python Developer. 
+# Unfortunately your team members seem to be quite old-school: 
+# you can see old-style string formatting everywhere in the code, 
+# which is not too cool. You tried to force the team members to 
+# start using the new style formatting, but it looks like it will 
+# take some time to persuade them: old habits die hard, especially in old-school programmers.
+
+# To show your colleagues that the new style formatting is not that 
+# different from the old style, you decided to implement a function 
+# that will turn the old-style syntax into a new one. Implement a 
+# function that will turn the old-style string formating s into a 
+# new one so that the following two strings have the same meaning:
+
+# s % (*args)
+# s.format(*args)
+# Example
+
+# For s = "We expect the %f%% growth this week", the output should be
+# newStyleFormatting(s) = "We expect the {}% growth this week".
+
+def newStyleFormatting(s):
+    s = s.replace('%%', '__',len(s))
+    s = re.sub('%([sbcdoXnfxgr])', '{}', s,count=50)
+    s = s.replace('__', '%',len(s))
+    return s
+
+# The Abanamama Version System (AVS) is a software versioning and revision 
+# control system used in highly secure environments. In this system, 
+# each commit is assigned a unique name, the first part of which consists 
+# of the username encrypted in the base-4 system using symbols '0', '?', '+', 
+# and '!', and the second part consists of symbols of English alphabet.
+
+# Given such commit, your task is go remove the username part from it 
+# and return the second part as an answer.
+
+# Example
+
+# For commit = "0??+0+!!someCommIdhsSt", the output should be
+# getCommit(commit) = "someCommIdhsSt".
+
+def getCommit(commit):
+   return re.sub('([0\+\?!]+)','',commit)
+
 def main():
-    pass
+    # feedback = "This is an example feedback"
+    # size = 8
+    # print(feedbackReview(feedback, size))
+    # t = 837.28472
+    # width = 20 
+    # precision = 7
+    # print(competitiveEating(t,width,precision))
+    # s = "New style formatting (like %s) is waay cooler than old-style (i.e. %s)"
+    # print(newStyleFormatting(s))
+    commit = "0??+0+!!"
+    print(getCommit(commit))
 
 if __name__ == '__main__':
     main()
